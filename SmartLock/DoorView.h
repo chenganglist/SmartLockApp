f4d466283f5a12d772a6b6021aa2aa77a704c23f
csis <csis@csisdemacbook-air.local>
2016-04-15 11:03:37 +0800

    添加蓝牙模块，准备重构代码

--- SmartLock/DoorView.h
+++ SmartLock/DoorView.h
@@ -7,7 +7,13 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <CoreBluetooth/CoreBluetooth.h>
+#import "BabyBluetooth.h"
+#import "PeripheralViewContriller.h"
+
 
 @interface DoorView : UIViewController
+
+@property (nonatomic, weak) IBOutlet UITableView *tableView;
 
 @end
