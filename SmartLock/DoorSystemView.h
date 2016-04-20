34019cbdc8e19744417ba7962a95bcbb1aee0dc0
csis <csis@csisdemacbook-air.local>
2016-04-20 09:50:38 +0800

    添加门禁系统页面，注意tableview的数据源问题可能导致界面崩溃

---
+++ SmartLock/DoorSystemView.h
@@ -0,0 +1,13 @@
+//
+//  DoorSystemView.h
+//  SmartLock
+//
+//  Created by csis on 16/4/20.
+//  Copyright © 2016年 csis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DoorSystemView : UIViewController
+
+@end
---
+++ SmartLock/DoorSystemView.m
@@ -0,0 +1,37 @@
+//
+//  DoorSystemView.m
+//  SmartLock
+//
+//  Created by csis on 16/4/20.
+//  Copyright © 2016年 csis. All rights reserved.
+//
+
+#import "DoorSystemView.h"
+
+@interface DoorSystemView ()
+
+@end
+
+@implementation DoorSystemView
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
