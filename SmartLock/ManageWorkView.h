6c542a94eae044cec706e36961cdeb541a4108d6
csis <csis@csisdemacbook-air.local>
2016-04-25 13:49:33 +0800

    将任务管理主界面替换成tableviewcontroller

--- SmartLock/ManageWorkView.h
+++ SmartLock/ManageWorkView.h
@@ -9,7 +9,7 @@
 #import <UIKit/UIKit.h>
 #import <AFNetworking.h>
 
-@interface ManageWorkView : UIViewController
+@interface ManageWorkView : UITableViewController
 {
     UITableView *workTable;
     NSArray *datalist;
